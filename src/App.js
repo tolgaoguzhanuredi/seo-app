eb3b80a388b7cba6e8728f53bf15d63ddeb1a2b2
tolgaoguzhanuredi <70442500+tolgaoguzhanuredi@users.noreply.github.com>
2022-12-20 11:13:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [seoWords, setSeoWords] = useState([]);
+  const [text, setText] = useState('');
+
+  const handleTextInput = (event) => {
+    setText(event.target.value);
+  }
+
+  const calculateTopWords = () => {
+    // split the text into an array of words
+    const words = text.split(' ');
+    // create an object to store the frequency of each word
+    const wordFrequency = {};
+    // iterate over the array of words and increment the count for each word in the object
+    for (const word of words) {
+      wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+    }
+    // get an array of key-value pairs from the object
+    const wordArray = Object.entries(wordFrequency);
+    // sort the array in descending order by the frequency of the words
+    wordArray.sort((a, b) => b[1] - a[1]);
+    // get the top 5 most frequent words
+    const topWords = wordArray.slice(0, 5);
+    // update the state with the top words
+    setSeoWords(topWords);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Your SEO Words</h1>
+      <textarea onChange={handleTextInput} />
+      <button onClick={calculateTopWords}>Calculate</button>
+      <ul>
+        {seoWords.map(([word, frequency]) => (
+          <li key={word}>{word}: {frequency}</li>
+        ))}
+      </ul>
     </div>
   );
 }
