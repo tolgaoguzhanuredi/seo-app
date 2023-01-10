39aa6070bb5f696dc4868218e756a6a3f288bb4c
tolgaoguzhanuredi <70442500+tolgaoguzhanuredi@users.noreply.github.com>
2023-01-10 22:48:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,70 @@
 import React, { useState } from 'react';
 
-function App() {
-  const [seoWords, setSeoWords] = useState([]);
-  const [text, setText] = useState('');
+const App = () => {
+    const [result, setResult] = useState({});
+    const [inputText, setInputText] = useState("");
 
-  const handleTextInput = (event) => {
-    setText(event.target.value);
-  }
+    const excludedWords = ["the", "and", "what", "he", "she", "it", "do", "does"];
 
-  const calculateTopWords = () => {
-    // split the text into an array of words
-    const words = text.split(' ');
-    // create an object to store the frequency of each word
-    const wordFrequency = {};
-    // iterate over the array of words and increment the count for each word in the object
-    for (const word of words) {
-      wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+    const handleInput = (event) => {
+        setInputText(event.target.value);
     }
-    // get an array of key-value pairs from the object
-    const wordArray = Object.entries(wordFrequency);
-    // sort the array in descending order by the frequency of the words
-    wordArray.sort((a, b) => b[1] - a[1]);
-    // get the top 5 most frequent words
-    const topWords = wordArray.slice(0, 5);
-    // update the state with the top words
-    setSeoWords(topWords);
-  }
 
-  return (
-    <div>
-      <h1>Your SEO Words</h1>
-      <textarea onChange={handleTextInput} />
-      <button onClick={calculateTopWords}>Calculate</button>
-      <ul>
-        {seoWords.map(([word, frequency]) => (
-          <li key={word}>{word}: {frequency}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+    const analyzeText = async () => {
+        const API_KEY = '38e2d3f94b1c497591e20b1d8332e2bd';
+        const endpoint = 'https://mycognitive8779.cognitiveservices.azure.com/';
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key': API_KEY,
+            },
+            body: JSON.stringify({
+                'documents': [
+                    {
+                        'language': 'en',
+                        'id': '1',
+                        'text': inputText,
+                    },
+                ],
+            }),
+        });
 
+        const data = await response.json();
+        setResult(data);
+    }
+
+    const getWords = () => {
+        if (!result.documents) return;
+        let wordFrequency = {};
+        result.documents[0].keyPhrases.forEach(word => {
+            wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+        });
+
+        // removing unwanted keywords 
+        for (let word of excludedWords) {
+            if (wordFrequency[word]) {
+                delete wordFrequency[word];
+            }
+        }
+
+        let sortedWords = Object.entries(wordFrequency)
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, 10);
+
+        return sortedWords;
+    }
+
+    return (
+        <div>
+            <h1>Your SEO Words</h1>
+            <textarea onChange={handleInput} />
+            <button onClick={analyzeText}>Submit</button>
+            <ul>
+            {getWords().map((word, index) => 
+              <li key={index}>{word[0]}: {word[1]}</li>)}
+            </ul>
+        </div>
+    );
+            }
 export default App;
